6be9537a33d1f7934d3d875a5c584dee68ab3c1d
AlexYang <m9939418@gmail.com>
2024-12-30 09:30:25 +0800

    Initial Commit

---
+++ AlexGitHubSwiftUI/AlexGitHubSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+// AlexGitHubSwiftUIApp.swift
+// AlexGitHubSwiftUI
+//
+// Created by AlexYang on 2024/12/30.
+//
+
+
+import SwiftUI
+
+@main
+struct AlexGitHubSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
